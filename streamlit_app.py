a1353b5a5ac5ff25d9b7345ccd6d2f5063df82a0
Lubaid Nabi Malik <142424005+lubaid-01@users.noreply.github.com>
2024-09-20 22:14:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ tf = pickle.load(open("vectorizer.pkl","rb"))
 model = pickle.load(open("model.pkl","rb"))
 
 st.title("EMAIL/SMS Sapm Classifier")
+st.write(<p style="color = red;"> "Caution: This is a basic spam classifier. It may not be 100% accurate. Always exercise caution and verify results before taking action." </p>)
 inp_msg = st.text_area("Enter message")
 
 
