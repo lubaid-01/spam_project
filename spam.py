ff2693762c5ba8d1a2b4cd734cbdac9b5b77f70e
Lubaid Nabi Malik <142424005+lubaid-01@users.noreply.github.com>
2024-09-20 17:27:59 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import  pickle
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk.corpus import stopwords
+import string
+import contractions
+import re
+'''nltk.download("stopwords")
+nltk.download("punkt_tab")
+nltk.download("wordnet")
+nltk.download("averaged_perceptron_tagger")'''
+lemmatizer= WordNetLemmatizer()
+
+def lemmatize_words(text):
+    lemma = None
+    tag = pos_tag([text])[0][1]
+    if tag.startswith('NN'):
+      lemma = lemmatizer.lemmatize(text, pos='n')
+    elif tag.startswith('VB'):
+      lemma = lemmatizer.lemmatize(text, pos='v')
+    elif tag.startswith('JJ'):
+      lemma = lemmatizer.lemmatize(text, pos='a')
+    elif tag.startswith('RB'):
+      lemma = lemmatizer.lemmatize(text, pos='r')
+    else:
+      lemma = lemmatizer.lemmatize(text)
+    return lemma
+stpwords = stopwords.words("english")
+def transform(li):
+  y = []
+  for i in li:
+    if i.isalnum() and i not in string.punctuation and i not in stpwords :
+          y.append(lemmatize_words(i))
+
+  return " ".join(y)
+tf = pickle.load(open("vectorizer.pkl","rb"))
+model = pickle.load(open("model.pkl","rb"))
+
+st.title("EMAIL/SMS Sapm Classifier")
+inp_msg = st.text_area("Enter message")
+
+
+#preprocess
+if st.button("Predict"):
+    inp_msg = nltk.word_tokenize(contractions.fix(re.sub(re.compile(r"https?://\S+|www\.\S+"),"",inp_msg).lower()))
+    inp_msg = transform(inp_msg)
+
+    in_vector = tf.transform([inp_msg]).toarray()
+    result = model.predict(in_vector)[0]
+    if result :
+      st.header("Spam")
+    else:
+      st.header("Not Spam")
