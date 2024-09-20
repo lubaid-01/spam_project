cea013736ac24c2e38c32ade19e1583dd2b30b29
Lubaid Nabi Malik <142424005+lubaid-01@users.noreply.github.com>
2024-09-20 19:36:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import re
 nltk.download("stopwords")
 nltk.download("punkt_tab")
 nltk.download("wordnet")
-nltk.download("averaged_perceptron_tagger")
+nltk.download("averaged_perceptron_tagger_eng")
 lemmatizer= WordNetLemmatizer()
 
 def lemmatize_words(text):
